f54ee97def6c0a9edd676dd791c8eb3751f6ce72
wchang28 <wchang28@hotmail.com>
2016-07-26 08:38:26 -0400

    nhedgtng

--- ts/services/index.ts
+++ ts/services/index.ts
@@ -5,7 +5,7 @@ import * as  oauth2 from 'oauth2';
 
 let router = express.Router();
 
-// all handlers in the '/authorize' need to have a x-client-app request header field
+// all handlers in the '/authorize' need to have a 'x-client-app' request header field
 let clientAppMiddleware = (req:express.Request, res:express.Response, next:express.NextFunction) => {
     let s = req.headers['x-client-app'];
     try {
