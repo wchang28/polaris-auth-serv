3896bc77aaba3eb64e195abf81cae851a984db79
wchang28 <wchang28@hotmail.com>
2016-07-27 09:54:51 -0400

    gfnsg

--- ts/services/authorize/index.ts
+++ ts/services/authorize/index.ts
@@ -72,9 +72,9 @@ let activeDirectoryLoginMiddleware = (req:express.Request, res:express.Response,
             auth.authenticate(params.username, params.password, (err, u) => {
                 auth.close((err:any) => {
                     if (err)
-                        console.error('!!! ADAuth closed error:' + JSON.stringify(err));
+                        console.error('!!! Error closing connection to the Active Directory server: ' + JSON.stringify(err));
                     else
-                        console.log('ADAuth closed successfully');
+                        console.log('connection to the Active Directory server closed successfully');
                 });
                 if (err)
                     res.status(401).json(oauth2.errors.bad_credential);
